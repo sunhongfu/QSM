217deea5dd1d0a0e9cd4fd76c424d753a9537706
rtopfer <topfer@ualberta.ca>
2013-05-08 13:35:27 -0600

    changed fftshift. hongfu wants consistency.

--- resizekspace.m
+++ resizekspace.m
@@ -88,11 +88,11 @@ if min( newGridDimensionVector ) < min( gridDimensionVector )
     for echo = 1 :numEcho
         for rx = 1 : numRx
             
-            fImg = fftshift( fftn( imgIn(:,:,:, echo, rx) ) ) ;
+            fImg = fftshift( fftc( imgIn(:,:,:, echo, rx) ) ) ;
             
             fImg = fImg(midPoint(1) - offset(1):midPoint(1) + offset(1), midPoint(2) - offset(2) : midPoint(2) + offset(2),midPoint(3) - offset(3) : midPoint(3) + offset(3));
             
-            img  = ifftn( ifftshift( fImg ) ) ;
+            img  = ifftc( ifftshift( fImg ) ) ;
             
             imgOut(:,:,:,echo,rx) = img ;
             
@@ -113,11 +113,11 @@ if max( newGridDimensionVector ) > max( gridDimensionVector )
     for echo = 1 :numEcho
         for rx = 1 : numRx
             
-            fImg = fftshift( fftn( imgIn(:,:,:, echo, rx) ) ) ;
+            fImg = fftshift( fftc( imgIn(:,:,:, echo, rx) ) ) ;
             
             fImg = padarray( fImg, padSize ) ;
             
-            img  = ifftn( ifftshift( fImg ) ) ;
+            img  = ifftc( ifftshift( fImg ) ) ;
             
             imgOut(:,:,:,echo,rx) = img ;
             
