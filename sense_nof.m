b9302640b2101d449fe1958e38e033281c47d9bd
hongfu <sunhongfu@gmail.com>
2013-05-09 14:59:14 -0600

    add different bkgrm methods

--- sense_nof.m
+++ sense_nof.m
@@ -32,12 +32,44 @@ unph_te1_cmb = unph_diff_cmb*TE1/(TE2-TE1);
 offsets = img(:,:,:,1,:)./repmat(exp(1j*unph_te1_cmb),[1,1,1,1,4]);
 offsets = offsets./abs(offsets);
 
+
+% (1) unwrap offset for each channel
+for chan = 1:nrcvrs
+    nii = make_nii(angle(offsets(:,:,:,1,chan)),res);
+    save_nii(nii,['offset_ch' num2str(chan) '.nii']);
+    nii = make_nii(abs(img(:,:,:,1,chan)),res);
+    save_nii(nii,['magTE1_ch' num2str(chan) '.nii']);
+end
+
+bash_command = sprintf(['for ph in offset_ch*\n' ...
+'do\n' ...
+'       base=`basename $ph`;\n' ...
+'       mag="magTE1_ch"${base:9};\n' ...
+'       unph="unoffset_ch"${base:9};\n' ...
+'       prelude -a $mag -p $ph -u $unph -m BET_mask.nii -n 8&\n' ...
+'done\n' ...
+'wait\n' ...
+'gunzip -f unoffset_ch*.gz\n']);
+
+unix(bash_command);
+
+unoffsets = zeros(size(offsets));
+med_unoffsets = unoffsets;
+for chan = 1:nrcvrs
+    nii = load_nii(['unoffset_ch' num2str(chan) '.nii']);
+    unoffsets(:,:,:,1,chan) = double(nii.img);
+    med_unoffsets(:,:,:,1,chan) = medfilt3(squeeze(unoffsets(:,:,:,1,chan)),4./res+1);
+    nii = make_nii(med_unoffsets(:,:,:,1,chan),res);
+    save_nii(nii,['medfilt3_offsets_ch' num2str(chan) '.nii']);
+end
+
+% (2) smooth in complex format
 %for chan = 1:nrcvrs
 %    offsets(:,:,:,:,chan) = smooth3(offsets(:,:,:,:,chan),'box',round(5./res/2)*2+1); % 5mm size kernel
 %    offsets(:,:,:,:,chan) = offsets(:,:,:,:,chan)./abs(offsets(:,:,:,:,chan));
 %end
 
 % combine phase according to complex summation
-offsets = repmat(offsets,[1,1,1,ne,1]);
-img = img./offsets;
+med_unoffsets = repmat(exp(1j*med_unoffsets),[1,1,1,ne,1]);
+img = img./med_unoffsets;
 ph_cmb = angle(sum(img,5));
---
+++ sharp.m
@@ -0,0 +1,55 @@
+function [ph_corr, mask_ero] = sharp(ph,mask,par,radius,t)
+%SHARP Background field removal using SHARP method.
+%   [PH_SHP,MASK_ERO] = sharp(PH,MASK,PAR,TSVD)
+%
+%   PH:       input total field
+%   MASK:     binary mask defining the ROI
+%   PAR:      parameter sets of the sequence
+%   TSVD:     truncation level for TSVD
+%   PH_SHP:   local field after background removal (SHARP)
+%   MASK_ERO: new ROI after erosion
+
+disp('SHARP removing background phase');
+
+% t: TSVD threshold
+
+% ph can be either 3D (np*nv*ns) or 4D (np*nv*ns*ne) or 5D (np*nv*ns*ne*nrcvrs)
+[np nv ns] = size(ph);
+res = [par.lro/(par.np/2), par.lpe/par.nv, par.lpe2/par.nv2]*10; % resolution pix/mm
+
+% make spherical/ellipsoidal convolution kernel
+rx = round(radius/res(1));
+ry = round(radius/res(2));
+rz = round(radius/res(3));
+[X,Y,Z] = ndgrid(-rx:rx,-ry:ry,-rz:rz);
+h = (X.^2/rx^2 + Y.^2/ry^2 + Z.^2/rz^2 < 1);
+ker = h/sum(h(:));
+
+% erode the mask
+mask_tmp = circshift(real(ifftn(fftn(mask).*fftn(ker,[np nv ns]))),[-rx,-ry,-rz]);
+mask_ero = zeros([np nv ns]);
+mask_ero(mask_tmp > 1-6/sum(h(:))) = 1; % 2 error points tolerance
+
+% prepare deconvolution kernel
+d_ker = -ker;
+d_ker(rx+1,ry+1,rz+1) = 1-ker(rx+1,ry+1,rz+1);
+D_ker = fftn(d_ker,[np nv ns]); % d_ker in Fourier domain
+
+% SHARP
+% convolute the total field (ext + int) with d_kernel
+ph_tmp = circshift(ifftn(fftn(ph).*D_ker),[-rx,-ry,-rz]);
+% erode the result (abandon brain edges)
+ph_tmp = ph_tmp.*mask_ero;
+% deconvolution
+PH_int = fftn(ph_tmp)./D_ker;
+PH_int(abs(D_ker)<t) = 0;
+ph_tmp = circshift(ifftn(PH_int),[rx,ry,rz]);
+ph_corr = real(ph_tmp).*mask_ero;
+
+
+% % %%
+% % % set the DC point of field in k-space to 0
+% % k = fftn(ph_corr);
+% % k(1,1,1) = 0;
+% % ph_corr = real(ifftn(k)).*mask_ero;
+% % %%
