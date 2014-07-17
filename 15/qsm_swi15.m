774856ebb764793fd8bde5e28ba38232c718b5de
megafeet <sunhongfu@gmail.com>
2014-07-17 16:40:53 -0600

    qsm_swi15: (1) ref_coil set to number 4 (2) smv radius set to 3 (3) LBV's eroded mask is wrong (fixed) (4) fix the rotation by z-projections

--- 15/qsm_swi15.m
+++ 15/qsm_swi15.m
@@ -7,14 +7,14 @@ function qsm_swi15(meas_in, path_out, options)
 %   MEAS_IN    - filename or directory of meas file(.out)  : *.out
 %   PATH_OUT   - directory to save nifti and/or matrixes   : QSM_*
 %   OPTIONS    - parameter structure including fields below
-%    .ref_coi  - reference coil to use for phase combine   : 3
+%    .ref_coi  - reference coil to use for phase combine   : 4
 %    .eig_rad  - radius (mm) of eig decomp kernel          : 4
-%    .smv_rad  - radius (mm) of SMV convolution kernel     : 6
+%    .smv_rad  - radius (mm) of SMV convolution kernel     : 3
 %    .tik_reg  - Tikhonov regularization for RESHARP       : 0.001
 %    .tv_reg   - Total variation regularization parameter  : 0.0005
 %    .bet_thr  - threshold for BET brain mask              : 0.4
 %    .tvdi_n   - iteration number of TVDI (nlcg)           : 200
-%    .sav_all  - save all the variables for debug          : 0
+%    .sav_all  - save all the variables for debug          : 1
 
 if ~ exist('meas_in','var') || isempty(meas_in)
 	listing = dir([pwd '/*.out']);
@@ -64,7 +64,7 @@ if ~ isfield(options,'bet_thr')
 end
 
 if ~ isfield(options,'smv_rad')
-    options.smv_rad = 6;
+    options.smv_rad = 3;
 end
 
 if ~ isfield(options,'tik_reg')
@@ -95,15 +95,13 @@ sav_all = options.sav_all;
 
 % define directories
 [~,name] = fileparts(filename);
-path_qsm = [path_out, filesep, 'lap_QSM_' name];
+path_qsm = [path_out, filesep, 'QSM_' name];
 mkdir(path_qsm);
 init_dir = pwd;
 cd(path_qsm);
-disp(['Start recon of ' filename]);
 
 
 % generate raw img
-disp('--> reconstruct to complex img ...');
 rawfile = {[pathstr,filesep],filename};
 [img,params] = swi15_recon(rawfile);
 
@@ -115,7 +113,6 @@ voxelSize = [FOV.dPhaseFOV/Npe, FOV.dReadoutFOV/Nro,  FOV.dThickness/Ns];
 
 
 % combine RF coils
-disp('--> combine RF rcvrs ...');
 img_cmb = sense_se(img,voxelSize,ref_coi,eig_rad);
 mkdir('combine');
 nii = make_nii(abs(img_cmb),voxelSize);
@@ -138,9 +135,7 @@ save_nii(nii,'combine/ph_cmb.nii');
 % save_nii(nii,'ph_cmb.nii');
 
 
-
 % generate brain mask
-disp('--> extract brain volume and generate mask ...');
 setenv('bet_thr',num2str(bet_thr));
 unix('bet combine/mag_cmb.nii BET -f ${bet_thr} -m -R');
 unix('gunzip -f BET.nii.gz');
@@ -156,6 +151,7 @@ mask = double(nii.img);
 %nii = load_nii('unph.nii');
 %unph = double(nii.img);
 
+
 % unwrap with Laplacian based method
 unph = unwrapLaplacian(angle(img_cmb), size(img_cmb), voxelSize);
 nii = make_nii(unph, voxelSize);
@@ -165,33 +161,39 @@ save_nii(nii,'unph_lap.nii');
 % nii = make_nii(unph, voxelSize);
 % save_nii(nii,'unph.nii');
 
-% background field removal
-%disp('--> RESHARP to remove background field ...');
-%mkdir('RESHARP');
-%[lph_resharp,mask_resharp] = resharp(unph,mask,voxelSize,smv_rad,tik_reg);
-
-%% LBV
-% lph_lbv = LBV(iFreq,Mask,matrix_size,voxel_size,tol,peel,depth,N1,N2,N3)
-lph_lbv = LBV(unph,mask,size(unph),voxelSize,0.01,2); % strip 2 layers
 
 % normalize to ppm unit
 TE = params.protocol_header.alTE{1}/1e6;
 B_0 = params.protocol_header.m_flMagneticFieldStrength;
 gamma = 2.675222e8;
+tfs = unph/(gamma*TE*B_0)*1e6; % unit ppm
 
-%lfs_resharp = lph_resharp/(gamma*TE*B_0)*1e6; % unit ppm
-%nii = make_nii(lfs_resharp,voxelSize);
-%save_nii(nii,'RESHARP/lfs_resharp.nii');
 
-lfs_lbv = lph_lbv/(gamma*TE*B_0)*1e6; % unit ppm
+% background field removal
+% (1) RESHARP
+[lfs_resharp,mask_resharp] = resharp(tfs,mask,voxelSize,smv_rad,tik_reg);
+mkdir('RESHARP');
+nii = make_nii(lfs_resharp,voxelSize);
+save_nii(nii,'RESHARP/lfs_resharp.nii');
+
+% (2) LBV
+lfs_lbv = LBV(tfs,mask,size(unph),voxelSize,0.01,2); % strip 2 layers
 mkdir('LBV');
 nii = make_nii(lfs_lbv,voxelSize);
 save_nii(nii,'LBV/lfs_lbv.nii');
+% % Don't use LBV's mask*.bin, not accurate
+% % read in eroded mask from LBV
+% listing = dir('mask*.bin');
+% filename = listing.name;
+% fid = fopen(filename);
+% mask_lbv = fread(fid,'int');
+% mask_lbv = reshape(mask_lbv,size(mask));
+% fclose all;
+mask_lbv = ones(size(mask));
+mask_lbv(lfs_lbv==0) = 0;
+
 
 % susceptibility inversion
-disp('--> TV susceptibility inversion ...');
-% account for oblique slicing (head tilted)
-% theta = -acos(params.protocol_header.sSliceArray.asSlice{1}.sNormal.dTra);
 sNormal = params.protocol_header.sSliceArray.asSlice{1}.sNormal;
 if ~ isfield(sNormal,'dSag')
     sNormal.dSag = 0;
@@ -211,25 +213,21 @@ end
 if ischar(sNormal.dTra)
     sNormal.dTra = 0;
 end
-nor_vec = [sNormal.dSag, sNormal.dCor, sNormal.dTra];
+z_prjs = [-sNormal.dSag, -sNormal.dCor, sNormal.dTra]
 
-%sus_resharp = tvdi(lfs_resharp,mask_resharp,voxelSize,tv_reg,abs(img_cmb),nor_vec,tvdi_n);
-%nii = make_nii(sus_resharp,voxelSize);
-%save_nii(nii,'RESHARP/sus_resharp.nii');
+% (1) RESHARP
+[sus_resharp,residual_resharp] = tvdi(lfs_resharp,mask_resharp,voxelSize,tv_reg,abs(img_cmb),z_prjs,tvdi_n);
+nii = make_nii(sus_resharp,voxelSize);
+save_nii(nii,'RESHARP/sus_resharp.nii');
+nii=make_nii(sus_resharp.*mask_resharp,voxelSize);
+save_nii(nii,'RESHARP/sus_resharp_clean.nii');
 
-
-% read in eroded mask from LBV
-listing = dir('mask*.bin');
-filename = listing.name;
-fid = fopen(filename);
-mask_lbv = fread(fid,'int');
-mask_lbv = reshape(mask_lbv,size(mask));
-fclose all;
-
-sus_lbv = tvdi(lfs_lbv,mask_lbv,voxelSize,tv_reg,abs(img_cmb),nor_vec,tvdi_n);
+% (2) LBV
+[sus_lbv,residual_lbv] = tvdi(lfs_lbv,mask_lbv,voxelSize,tv_reg,abs(img_cmb),z_prjs,tvdi_n);
 nii = make_nii(sus_lbv,voxelSize);
-save_nii(nii,'LBV/sus_lbv_FROMnormal.nii');
-
+save_nii(nii,'LBV/sus_lbv.nii');
+nii=make_nii(sus_lbv.*mask_lbv,voxelSize);        
+save_nii(nii,'LBV/sus_lbv_clean.nii');  
 
 
 % save all variables for debugging purpose
