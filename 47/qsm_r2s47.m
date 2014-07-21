3d8c3cdd2692bd76be8931f70378479c62e6e95e
megafeet <sunhongfu@gmail.com>
2014-07-21 12:17:47 -0600

    qsm_r2s47: reverted back to 5 echoes, sense_me and prelude...

--- 47/qsm_r2s47.m
+++ 47/qsm_r2s47.m
@@ -102,10 +102,14 @@ cd(path_qsm);
 disp('--> reconstruct fid to complex img ...');
 [img,par] = r2s47_recon(path_fid);
 
-% % keep only the first 'echo_t' echoes
-% % img_orig = img;
-% img = img(:,:,:,1:echo_t,:);
-% par.ne = echo_t;
+
+%%%%%%%%%%%%% old %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% keep only the first 'echo_t' echoes
+% img_orig = img;
+img = img(:,:,:,1:echo_t,:);
+par.ne = echo_t;
+%%%%%%%%%%%%% old %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 % swap the first two dimensions and match scanner frame
 img = permute(img,[2 1 3 4 5]); %PE,RO,SL,NE,RX
@@ -115,305 +119,283 @@ par.res = [par.res(2), par.res(1), par.res(3)];
 [nv,np,nv2,ne,~] = size(img);
 voxelSize = par.res; % resolution in mm/pixel
 
-phi = par.phi/180*pi
-psi = par.psi/180*pi
-theta = -par.theta/180*pi
-z_prjs = [sin(psi)*sin(theta), cos(psi)*sin(theta), cos(theta)] 
-
-
-if par.nrcvrs > 1
-    % combine using eig method
-    img_cmb = sense_se(permute(img,[1 2 3 5 4]),voxelSize,3,3);
-else
-    img_cmb = img;
-end
-
-mkdir('combine');
-for echo = 1:size(img_cmb,4)
-    nii = make_nii(abs(img_cmb(:,:,:,echo)),voxelSize);
-    save_nii(nii,['combine/mag_cmb' num2str(echo) '.nii']);
-    nii = make_nii(angle(img_cmb(:,:,:,echo)),voxelSize);
-    save_nii(nii,['combine/ph_cmb' num2str(echo) '.nii']);
-end
+phi = par.phi/180*pi;
+psi = par.psi/180*pi;
+theta = -par.theta/180*pi;
+z_prjs = [sin(psi)*sin(theta), cos(psi)*sin(theta), cos(theta)]; 
 
 
 
-
-%% generate mask from combined magnitude of the first echo
-disp('--> extract brain volume and generate mask ...');
-setenv('bet_thr',num2str(bet_thr));
-unix('bet combine/mag_cmb1.nii BET -f ${bet_thr} -m -R');
-unix('gunzip -f BET.nii.gz');
-unix('gunzip -f BET_mask.nii.gz');
-nii = load_nii('BET_mask.nii');
-mask = double(nii.img);
-
-
-% fit the echoes complexly
-[p1, dp1, relres, p0]=Fit_ppm_complex(img_cmb);
-nii = make_nii(p1,voxelSize);
-save_nii(nii,'complex_fit_p1.nii');
-
-
-% unwrap using laplacian
-unph = unwrapLaplacian(p1, size(p1), voxelSize);
-nii = make_nii(unph, voxelSize);
-save_nii(nii,'unph_lap.nii');
-
-%convert x to ppm
-tfs = unph/(2*pi*par.esp*42.576*4.7*1e6)*1e6;
-nii = make_nii(tfs, voxelSize);
-save_nii(nii,'tfs_ppm.nii');
-
-
-% RESHARP
-relres_blur = smooth3(relres,'box',round(6./par.res/2)*2+1); 
-nii = make_nii(relres_blur,voxelSize);
-save_nii(nii,'relres_blur.nii');
-
-R = ones(size(relres_blur));
-R(relres_blur >= 0.2) = 0;
-
-[lfs_resharp, mask_resharp] = resharp(tfs,mask.*R,voxelSize,smv_rad,tik_reg);
-mkdir('RESHARP');
-nii = make_nii(lfs_resharp,voxelSize);
-save_nii(nii,'RESHARP/lfs_resharp.nii');
-
-
-% inversion of susceptibility 
-disp('--> TV susceptibility inversion on RESHARP...');
-[sus_resharp,residual_resharp] = tvdi(lfs_resharp,mask_resharp,voxelSize,tv_reg,abs(img_cmb(:,:,:,end)),z_prjs,tvdi_n);
-
-
-% save nifti
-nii = make_nii(sus_resharp,voxelSize);
-save_nii(nii,'RESHARP/sus_resharp.nii');
-
-% save nifti
-nii = make_nii(sus_resharp.*mask_resharp,voxelSize);
-save_nii(nii,'RESHARP/sus_resharp_clean.nii');
-
-
-
-
-% % RESHARP each echo
-% lph_resharp = zeros(size(img_cmb));
-% mask_resharp = zeros(size(img_cmb));
-% for echo = 1:size(img_cmb,4)
-%     [lph_resharp(:,:,:,echo), mask] = resharp(unph(:,:,:,echo),mask(:,:,:,echo),voxelSize,smv_rad,tik_reg);
+%%%%%%%%%%%%%%%%%% new %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% if par.nrcvrs > 1
+%     % combine using eig method
+%     img_cmb = sense_se(permute(img,[1 2 3 5 4]),voxelSize,3,3);
+% else
+%     img_cmb = img;
 % end
 
-% mkdir('RESHARP');
-% nii = make_nii(lph_resharp,voxelSize);
-% save_nii(nii,'RESHARP/lph_resharp.nii');
-
-
-% %% fit phase images with echo times
-% disp('--> magnitude weighted LS fit of phase to TE ...');
-% [lfs_resharp, fit_residual] = echofit(lph_resharp,abs(img_cmb),par); 
-
-% nii = make_nii(lfs_resharp,voxelSize);
-% save_nii(nii,'RESHARP/lfs_resharp.nii');
-% nii = make_nii(fit_residual,voxelSize);
-% save_nii(nii,'RESHARP/fit_residual.nii');
-
-
-
-
-% %% combine magnitude channels using marc's 'arrayrec.m'
-% disp('--> combine rcvrs for magnitude ...');
-% mag_cmb = zeros(nv,np,nv2,ne);
-% for echo = 1:ne
-%     mag_cmb(:,:,:,echo) = arrayrec(squeeze(img(:,:,:,echo,:)),1/2);
-% end
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % TO DO: remove coil sense profile (Hammond paper)
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% % save nifti for unwrapping usage
 % mkdir('combine');
-% for echo =  1:ne
-%     nii = make_nii(mag_cmb(:,:,:,echo),voxelSize);
-%     save_nii(nii,['combine/mag_te' num2str(echo) '.nii']);
+% for echo = 1:size(img_cmb,4)
+%     nii = make_nii(abs(img_cmb(:,:,:,echo)),voxelSize);
+%     save_nii(nii,['combine/mag_cmb' num2str(echo) '.nii']);
+%     nii = make_nii(angle(img_cmb(:,:,:,echo)),voxelSize);
+%     save_nii(nii,['combine/ph_cmb' num2str(echo) '.nii']);
 % end
 
 
 % %% generate mask from combined magnitude of the first echo
 % disp('--> extract brain volume and generate mask ...');
 % setenv('bet_thr',num2str(bet_thr));
-% unix('bet combine/mag_te1.nii BET -f ${bet_thr} -m -R');
+% unix('bet combine/mag_cmb1.nii BET -f ${bet_thr} -m -R');
 % unix('gunzip -f BET.nii.gz');
 % unix('gunzip -f BET_mask.nii.gz');
 % nii = load_nii('BET_mask.nii');
 % mask = double(nii.img);
 
 
-% %% combine phase channels
-% disp('--> combine rcvrs for phase ...');
-% ph_cmb = sense_me(img,par);
-
-% % save nifti for unwrapping usage
-% for echo =  1:ne
-%     nii = make_nii(ph_cmb(:,:,:,echo),voxelSize);
-%     save_nii(nii,['combine/ph_te' num2str(echo) '.nii']);
-% end
-
-% clear img;
+% % fit the echoes complexly
+% [p1, dp1, relres, p0]=Fit_ppm_complex(img_cmb);
+% nii = make_nii(p1,voxelSize);
+% save_nii(nii,'complex_fit_p1.nii');
 
 
-% %% unwrap phase from each echo
-% disp('--> unwrap aliasing phase for all TEs ...');
+% % unwrap using laplacian
+% unph = unwrapLaplacian(p1, size(p1), voxelSize);
+% nii = make_nii(unph, voxelSize);
+% save_nii(nii,'unph_lap.nii');
 
-% bash_command = sprintf(['for ph in combine/ph*\n' ...
-% 'do\n' ...
-% '	base=`basename $ph`;\n' ...
-% '	dir=`dirname $ph`;\n' ...
-% '	mag=$dir/"mag"${base:2};\n' ...
-% '	unph="unph"${base:2};\n' ...
-% '	prelude -a $mag -p $ph -u $unph -m BET_mask.nii -n 8&\n' ...
-% 'done\n' ...
-% 'wait\n' ...
-% 'gunzip -f unph*.gz\n']);
-
-% unix(bash_command);
-
-% unph_cmb = zeros(nv,np,nv2,ne);
-% for echo = 1:ne
-%     nii = load_nii(['unph_te' num2str(echo) '.nii']);
-%     unph_cmb(:,:,:,echo) = double(nii.img);
-% end
+% %convert x to ppm
+% tfs = unph/(2*pi*par.esp*42.576*4.7*1e6)*1e6;
+% nii = make_nii(tfs, voxelSize);
+% save_nii(nii,'tfs_ppm.nii');
 
 
-% % check and correct for 2pi jump between echoes
-% disp('--> correct for potential 2pi jumps between TEs ...')
+% % RESHARP
+% relres_blur = smooth3(relres,'box',round(6./par.res/2)*2+1); 
+% nii = make_nii(relres_blur,voxelSize);
+% save_nii(nii,'relres_blur.nii');
 
-% nii = load_nii('unph_diff.nii');
-% unph_diff = double(nii.img);
+% R = ones(size(relres_blur));
+% R(relres_blur >= 0.2) = 0;
 
-% for echo = 2:ne
-%     meandiff = unph_cmb(:,:,:,echo)-unph_cmb(:,:,:,1)-(echo-1)*unph_diff;
-%     meandiff = meandiff(mask==1);
-%     meandiff = mean(meandiff(:));
-%     njump = round(meandiff/(2*pi));
-%     disp(['    ' num2str(njump) ' 2pi jumps for TE' num2str(echo)]);
-%     unph_cmb(:,:,:,echo) = unph_cmb(:,:,:,echo) - njump*2*pi;
-% end
+% [lfs_resharp, mask_resharp] = resharp(tfs,mask.*R,voxelSize,smv_rad,tik_reg);
+% mkdir('RESHARP');
+% nii = make_nii(lfs_resharp,voxelSize);
+% save_nii(nii,'RESHARP/lfs_resharp.nii');
 
 
-% %% fit phase images with echo times
-% disp('--> magnitude weighted LS fit of phase to TE ...');
-% [tfs, fit_residual] = echofit(unph_cmb,mag_cmb,par); 
-
-% % normalize to main field
-% % ph = gamma*dB*TE
-% % dB/B = ph/(gamma*TE*B0)
-% % units: TE s, gamma 2.675e8 rad/(sT), B0 4.7T
-% tfs = -tfs/(2.675e8*4.7)*1e6; % unit ppm
-
-% % generate reliability map
-% R = ones(size(fit_residual));
-% R(fit_residual >= fit_t) = 0;
+% % inversion of susceptibility 
+% disp('--> TV susceptibility inversion on RESHARP...');
+% [sus_resharp,residual_resharp] = tvdi(lfs_resharp,mask_resharp,voxelSize,tv_reg,abs(img_cmb(:,:,:,end)),z_prjs,tvdi_n);
 
 
-% %% PDF
-% if sum(strcmpi('pdf',bkgrm))
-%     disp('--> PDF to remove background field ...');
-%     [lfs_pdf,mask_pdf] = pdf(tfs,mask.*R,voxelSize,smv_rad,mag_cmb(:,:,:,echo_t));
+% % save nifti
+% nii = make_nii(sus_resharp,voxelSize);
+% save_nii(nii,'RESHARP/sus_resharp.nii');
 
-%     % save nifti
-%     mkdir('PDF');
-%     nii = make_nii(lfs_pdf,voxelSize);
-%     save_nii(nii,'PDF/lfs_pdf.nii');
-
-%     % inversion of susceptibility 
-%     disp('--> TV susceptibility inversion on PDF...');
-%     sus_pdf = tvdi(lfs_pdf, mask_pdf, voxelSize, tv_reg, mag_cmb(:,:,:,echo_t), z_prjs, tvdi_n); 
-
-%     % save nifti
-%     nii = make_nii(sus_pdf,voxelSize);
-%     save_nii(nii,'PDF/sus_pdf.nii');
-% end
+% % save nifti
+% nii = make_nii(sus_resharp.*mask_resharp,voxelSize);
+% save_nii(nii,'RESHARP/sus_resharp_clean.nii');
 
 
-% %% SHARP (tsvd: truncation threthold for TSVD)
-% if sum(strcmpi('sharp',bkgrm))
-%     disp('--> SHARP to remove background field ...');
-%     [lfs_sharp, mask_sharp] = sharp(tfs,mask.*R,voxelSize,smv_rad,tsvd);
 
-%     % save nifti
-%     mkdir('SHARP');
-%     nii = make_nii(lfs_sharp,voxelSize);
-%     save_nii(nii,'SHARP/lfs_sharp.nii');
+
+
+
+%%%%%%%%%%%%%%%%%%%% old %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% combine magnitude channels using marc's 'arrayrec.m'
+disp('--> combine rcvrs for magnitude ...');
+mag_cmb = zeros(nv,np,nv2,ne);
+for echo = 1:ne
+    mag_cmb(:,:,:,echo) = arrayrec(squeeze(img(:,:,:,echo,:)),1/2);
+end
+
+
+% save nifti for unwrapping usage
+mkdir('combine');
+for echo =  1:ne
+    nii = make_nii(mag_cmb(:,:,:,echo),voxelSize);
+    save_nii(nii,['combine/mag_te' num2str(echo) '.nii']);
+end
+
+
+%% generate mask from combined magnitude of the first echo
+disp('--> extract brain volume and generate mask ...');
+setenv('bet_thr',num2str(bet_thr));
+unix('bet combine/mag_te1.nii BET -f ${bet_thr} -m -R');
+unix('gunzip -f BET.nii.gz');
+unix('gunzip -f BET_mask.nii.gz');
+nii = load_nii('BET_mask.nii');
+mask = double(nii.img);
+
+
+%% combine phase channels
+disp('--> combine rcvrs for phase ...');
+ph_cmb = sense_me(img,par);
+
+% save nifti for unwrapping usage
+for echo =  1:ne
+    nii = make_nii(ph_cmb(:,:,:,echo),voxelSize);
+    save_nii(nii,['combine/ph_te' num2str(echo) '.nii']);
+end
+
+clear img;
+
+
+%% unwrap phase from each echo
+disp('--> unwrap aliasing phase for all TEs ...');
+
+bash_command = sprintf(['for ph in combine/ph*\n' ...
+'do\n' ...
+'	base=`basename $ph`;\n' ...
+'	dir=`dirname $ph`;\n' ...
+'	mag=$dir/"mag"${base:2};\n' ...
+'	unph="unph"${base:2};\n' ...
+'	prelude -a $mag -p $ph -u $unph -m BET_mask.nii -n 8&\n' ...
+'done\n' ...
+'wait\n' ...
+'gunzip -f unph*.gz\n']);
+
+unix(bash_command);
+
+unph_cmb = zeros(nv,np,nv2,ne);
+for echo = 1:ne
+    nii = load_nii(['unph_te' num2str(echo) '.nii']);
+    unph_cmb(:,:,:,echo) = double(nii.img);
+end
+
+
+% check and correct for 2pi jump between echoes
+disp('--> correct for potential 2pi jumps between TEs ...')
+
+nii = load_nii('unph_diff.nii');
+unph_diff = double(nii.img);
+
+for echo = 2:ne
+    meandiff = unph_cmb(:,:,:,echo)-unph_cmb(:,:,:,1)-(echo-1)*unph_diff;
+    meandiff = meandiff(mask==1);
+    meandiff = mean(meandiff(:));
+    njump = round(meandiff/(2*pi));
+    disp(['    ' num2str(njump) ' 2pi jumps for TE' num2str(echo)]);
+    unph_cmb(:,:,:,echo) = unph_cmb(:,:,:,echo) - njump*2*pi;
+end
+
+
+%% fit phase images with echo times
+disp('--> magnitude weighted LS fit of phase to TE ...');
+[tfs, fit_residual] = echofit(unph_cmb,mag_cmb,par); 
+
+% normalize to main field
+% ph = gamma*dB*TE
+% dB/B = ph/(gamma*TE*B0)
+% units: TE s, gamma 2.675e8 rad/(sT), B0 4.7T
+tfs = -tfs/(2.675e8*4.7)*1e6; % unit ppm
+
+% generate reliability map
+R = ones(size(fit_residual));
+R(fit_residual >= fit_t) = 0;
+
+
+%% PDF
+if sum(strcmpi('pdf',bkgrm))
+    disp('--> PDF to remove background field ...');
+    [lfs_pdf,mask_pdf] = pdf(tfs,mask.*R,voxelSize,smv_rad,mag_cmb(:,:,:,echo_t));
+
+    % save nifti
+    mkdir('PDF');
+    nii = make_nii(lfs_pdf,voxelSize);
+    save_nii(nii,'PDF/lfs_pdf.nii');
+
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on PDF...');
+    sus_pdf = tvdi(lfs_pdf, mask_pdf, voxelSize, tv_reg, mag_cmb(:,:,:,echo_t), z_prjs, tvdi_n); 
+
+    % save nifti
+    nii = make_nii(sus_pdf.*mask_pdf,voxelSize);
+    save_nii(nii,'PDF/sus_pdf.nii');
+end
+
+
+%% SHARP (tsvd: truncation threthold for TSVD)
+if sum(strcmpi('sharp',bkgrm))
+    disp('--> SHARP to remove background field ...');
+    [lfs_sharp, mask_sharp] = sharp(tfs,mask.*R,voxelSize,smv_rad,tsvd);
+
+    % save nifti
+    mkdir('SHARP');
+    nii = make_nii(lfs_sharp,voxelSize);
+    save_nii(nii,'SHARP/lfs_sharp.nii');
     
-%     % inversion of susceptibility 
-%     disp('--> TV susceptibility inversion on SHARP...');
-%     sus_sharp = tvdi(lfs_sharp, mask_sharp, voxelSize, tv_reg, mag_cmb(:,:,:,echo_t), z_prjs, tvdi_n); 
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on SHARP...');
+    sus_sharp = tvdi(lfs_sharp, mask_sharp, voxelSize, tv_reg, mag_cmb(:,:,:,echo_t), z_prjs, tvdi_n); 
    
-%     % save nifti
-%     nii = make_nii(sus_sharp,voxelSize);
-%     save_nii(nii,'SHARP/sus_sharp.nii');
-% end
+    % save nifti
+    nii = make_nii(sus_shar.*mask_sharp,voxelSize);
+    save_nii(nii,'SHARP/sus_sharp.nii');
+end
 
 
-% %% RE-SHARP (tik_reg: Tikhonov regularization parameter)
-% if sum(strcmpi('resharp',bkgrm))
-%     disp('--> RESHARP to remove background field ...');
-%     [lfs_resharp, mask_resharp] = resharp(tfs,mask.*R,voxelSize,smv_rad,tik_reg);
+%% RE-SHARP (tik_reg: Tikhonov regularization parameter)
+if sum(strcmpi('resharp',bkgrm))
+    disp('--> RESHARP to remove background field ...');
+    [lfs_resharp, mask_resharp] = resharp(tfs,mask.*R,voxelSize,smv_rad,tik_reg);
 
 
-%     % save nifti
-%     mkdir('RESHARP');
-%     nii = make_nii(lfs_resharp,voxelSize);
-%     save_nii(nii,'RESHARP/lfs_resharp.nii');
+    % save nifti
+    mkdir('RESHARP');
+    nii = make_nii(lfs_resharp,voxelSize);
+    save_nii(nii,'RESHARP/lfs_resharp.nii');
 
 
-%     % inversion of susceptibility 
-%     disp('--> TV susceptibility inversion on RESHARP...');
-%     sus_resharp = tvdi(lfs_resharp, mask_resharp, voxelSize, tv_reg, mag_cmb(:,:,:,echo_t), z_prjs, tvdi_n); 
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on RESHARP...');
+    sus_resharp = tvdi(lfs_resharp, mask_resharp, voxelSize, tv_reg, mag_cmb(:,:,:,echo_t), z_prjs, tvdi_n); 
    
 
+    % save nifti
+    nii = make_nii(sus_resharp.*mask_resharp,voxelSize);
+    save_nii(nii,'RESHARP/sus_resharp.nii');
+
+end
+
+
+%% E-SHARP
+% if sum(strcmpi('esharp',bkgrm))
+%     disp('--> (8/9) E-SHARP to remove background field ...');
+%     Options.voxelSize = voxelSize;
+%     lfs = esharp(tfs,mask.*R,Options);
+%     mask_final = mask.*R;
+% 
+% 
 %     % save nifti
-%     nii = make_nii(sus_resharp,voxelSize);
-%     save_nii(nii,'RESHARP/sus_resharp.nii');
-
+%     mkdir([path_qsm '/rmbkg/']);
+%     nii = make_nii(lfs,voxelSize);
+%     save_nii(nii,[path_qsm '/rmbkg/lfs_esharp_xy.nii']);
+%     nii = make_nii(permute(lfs,[1 3 2]),voxelSize);
+%     save_nii(nii,[path_qsm '/rmbkg/lfs_esharp_xz.nii']);
+%     nii = make_nii(permute(lfs,[2 3 1]),voxelSize);
+%     save_nii(nii,[path_qsm '/rmbkg/lfs_esharp_yz.nii']);
+%     nii = make_nii(mask_final,voxelSize);
+%     save_nii(nii,[path_qsm '/mask/mask_esharp_final.nii']);
+% 
+% 
+%     % inversion of susceptibility 
+%     disp('--> (9/9) TV susceptibility inversion on E-SHARP...');
+%     sus = tvdi(lfs, mask_final, voxelSize, tv_reg, mag_cmb(:,:,:,4)); 
+% 
+% 
+%     % save nifti
+%     mkdir([path_qsm '/inversion']);
+%     nii = make_nii(sus,voxelSize);
+%     save_nii(nii,[path_qsm '/inversion/sus_esharp_xy.nii']);
+%     nii = make_nii(permute(sus,[1 3 2]),voxelSize);
+%     save_nii(nii,[path_qsm '/inversion/sus_esharp_xz.nii']);
+%     nii = make_nii(permute(sus,[2 3 1]),voxelSize);
+%     save_nii(nii,[path_qsm '/inversion/sus_esharp_yz.nii']);
 % end
-
-
-% %% E-SHARP
-% % if sum(strcmpi('esharp',bkgrm))
-% %     disp('--> (8/9) E-SHARP to remove background field ...');
-% %     Options.voxelSize = voxelSize;
-% %     lfs = esharp(tfs,mask.*R,Options);
-% %     mask_final = mask.*R;
-% % 
-% % 
-% %     % save nifti
-% %     mkdir([path_qsm '/rmbkg/']);
-% %     nii = make_nii(lfs,voxelSize);
-% %     save_nii(nii,[path_qsm '/rmbkg/lfs_esharp_xy.nii']);
-% %     nii = make_nii(permute(lfs,[1 3 2]),voxelSize);
-% %     save_nii(nii,[path_qsm '/rmbkg/lfs_esharp_xz.nii']);
-% %     nii = make_nii(permute(lfs,[2 3 1]),voxelSize);
-% %     save_nii(nii,[path_qsm '/rmbkg/lfs_esharp_yz.nii']);
-% %     nii = make_nii(mask_final,voxelSize);
-% %     save_nii(nii,[path_qsm '/mask/mask_esharp_final.nii']);
-% % 
-% % 
-% %     % inversion of susceptibility 
-% %     disp('--> (9/9) TV susceptibility inversion on E-SHARP...');
-% %     sus = tvdi(lfs, mask_final, voxelSize, tv_reg, mag_cmb(:,:,:,4)); 
-% % 
-% % 
-% %     % save nifti
-% %     mkdir([path_qsm '/inversion']);
-% %     nii = make_nii(sus,voxelSize);
-% %     save_nii(nii,[path_qsm '/inversion/sus_esharp_xy.nii']);
-% %     nii = make_nii(permute(sus,[1 3 2]),voxelSize);
-% %     save_nii(nii,[path_qsm '/inversion/sus_esharp_xz.nii']);
-% %     nii = make_nii(permute(sus,[2 3 1]),voxelSize);
-% %     save_nii(nii,[path_qsm '/inversion/sus_esharp_yz.nii']);
-% % end
+%%%%%%%%%%%%%%%%%%%% old %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %% save all variables for debugging purpose
