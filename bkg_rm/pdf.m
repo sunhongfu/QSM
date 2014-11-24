02463bd13f96dc9b3e9c77c4ba42da53e835cba0
Hongfu Sun <sunhongfu@gmail.com>
2014-11-23 23:13:11 -0700

    update pdf.m to 7 points error tolerance in mask erosion

--- bkg_rm/pdf.m
+++ bkg_rm/pdf.m
@@ -63,7 +63,7 @@ csh = [rx,ry,rz];
 imsize = size(mask);
 mask_tmp = circshift(real(ifftn(fftn(mask).*fftn(ker,imsize))),-csh);
 mask_ero = zeros(imsize);
-mask_ero(mask_tmp > 1-1/sum(h(:))) = 1; % no error tolerance
+mask_ero(mask_tmp > 1-8/sum(h(:))) = 1; % no error tolerance
 lfs = lfs.*mask_ero;
 
 
