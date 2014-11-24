efe4a414f1f30954f152508c356b1c9c3f298507
Hongfu Sun <sunhongfu@gmail.com>
2014-11-23 23:26:43 -0700

    update 'sharp.m'

--- bkg_rm/sharp.m
+++ bkg_rm/sharp.m
@@ -1,20 +1,22 @@
-function [ph_corr, mask_ero] = sharp(ph,mask,res,radius,tsvd)
-%SHARP Background field removal using SHARP method.
-%   [PH_SHP,MASK_ERO] = sharp(PH,MASK,PAR,TSVD)
+function [lfs, mask_ero] = sharp(tfs,mask,vox,ker_rad,tsvd)
+%SHARP Background field removal
+%   [LSF,MASK_ERO] = SHARP(TFS,MASK,VOX,KER_RAD,TSVD)
 %
-%   PH:       input total field
-%   MASK:     binary mask defining the ROI
-%   RES:      resolution of the images (vector)
-%   TSVD:     truncation level for TSVD
-%   PH_SHP:   local field after background removal (SHARP)
-%   MASK_ERO: new ROI after erosion
+%   LFS         : local field shift after background removal
+%   MASK_ERO    : eroded mask after convolution
+%   TFS         : input total field shift
+%   MASK        : binary mask defining the brain ROI
+%   VOX         : voxel size (mm), e.g. [1,1,1] for isotropic
+%   KER_RAD     : radius of convolution kernel (mm), e.g. 5
+%   TSVD        : truncated singular value decomposition. e.g. 0.05
 
-imsize = size(ph);
+imsize = size(tfs);
 
-% make spherical/ellipsoidal convolution kernel
-rx = round(radius/res(1));
-ry = round(radius/res(2));
-rz = round(radius/res(3));
+% make spherical/ellipsoidal convolution kernel (ker)
+rx = round(ker_rad/vox(1));
+ry = round(ker_rad/vox(2));
+rz = round(ker_rad/vox(3));
+% rz = ceil(ker_rad/vox(3));
 [X,Y,Z] = ndgrid(-rx:rx,-ry:ry,-rz:rz);
 h = (X.^2/rx^2 + Y.^2/ry^2 + Z.^2/rz^2 < 1);
 ker = h/sum(h(:));
@@ -27,21 +29,23 @@ cvsize = imsize + [2*rx+1, 2*ry+1, 2*rz+1] -1; % linear conv size
 mask_tmp = real(ifftn(fftn(mask,cvsize).*fftn(ker,cvsize)));
 mask_tmp = mask_tmp(rx+1:end-rx, ry+1:end-ry, rz+1:end-rz); % same size
 mask_ero = zeros(imsize);
-mask_ero(mask_tmp > 1-6/sum(h(:))) = 1; % 5 points error tolerance
+mask_ero(mask_tmp > 1-8/sum(h(:))) = 1; % 7 error points tolerance
+
 
 % prepare convolution kernel: delta-ker
 dker = -ker;
 dker(rx+1,ry+1,rz+1) = 1-ker(rx+1,ry+1,rz+1);
-D_ker = fftn(dker,imsize); % dker in Fourier domain
+DKER = fftn(dker,imsize); % dker in Fourier domain
+
 
 % SHARP
 % convolute the total field (ext + int) with d_kernel
-ph_tmp = circshift(ifftn(fftn(ph).*D_ker),-csh);
+ph_tmp = circshift(ifftn(fftn(tfs).*D_ker),-csh);
 % erode the result (abandon brain edges)
 ph_tmp = ph_tmp.*mask_ero;
 % deconvolution
-PH_int = fftn(ph_tmp)./D_ker;
-PH_int(abs(D_ker)<tsvd) = 0;
-ph_tmp = circshift(ifftn(PH_int),csh);
-ph_corr = real(ph_tmp).*mask_ero;
+ph_int = fftn(ph_tmp)./D_ker;
+ph_int(abs(D_ker)<tsvd) = 0;
+ph_tmp = circshift(ifftn(ph_int),csh);
+lfs = real(ph_tmp).*mask_ero;
 
