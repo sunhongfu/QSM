8d42b6ae84ba059d2e2bd4c95dba58365d5c8e86
megafeet <sunhongfu@gmail.com>
2015-01-09 15:20:05 -0700

    add phase unwrapping/background phase removal options to qsm_swi15

--- 15/qsm_swi15.m
+++ 15/qsm_swi15.m
@@ -5,18 +5,23 @@ function qsm_swi15(meas_in, path_out, options)
 %   Re-define the following default settings if necessary
 %
 %   MEAS_IN     - filename or directory of meas file(.out)  : *.out
-%   PATH_OUT    - directory to save nifti and/or matrixes   : QSM_*
+%   PATH_OUT    - directory to save nifti and/or matrixes   : QSM_SWI15_v5
 %   OPTIONS     - parameter structure including fields below
-%    .ref_coi   - reference coil to use for phase combine   : 4
+%    .ref_coil  - reference coil to use for phase combine   : 1
 %    .eig_rad   - radius (mm) of eig decomp kernel          : 4
-%    .ph_unwrap - 'prelude' or 'laplacian' or 'bestpath'    : 'prelude'
-%    .smv_rad   - radius (mm) of SMV convolution kernel     : 3
-%    .tik_reg   - Tikhonov regularization for RESHARP       : 0.001
-%    .tv_reg    - Total variation regularization parameter  : 0.0005
 %    .bet_thr   - threshold for BET brain mask              : 0.4
-%    .inv_num   - iteration number of TVDI (nlcg)           : 200
-%    .save_all  - save all the variables for debug          : 1
+%    .ph_unwrap - 'prelude' or 'laplacian' or 'bestpath'    : 'prelude'
+%    .bkg_rm    - background field removal method(s)        : 'resharp'
+%    .smv_rad   - radius (mm) of SMV convolution kernel     : 4
+%    .tik_reg   - Tikhonov regularization for RESHARP       : 0.001
+%    .t_svd     - truncation of SVD for SHARP               : 0.05
+%    .lbv_layer - number of layers to be stripped off LBV   : 2
+%    .tv_reg    - Total variation regularization parameter  : 0.0005
+%    .tvdi_n    - iteration number of TVDI (nlcg)           : 200
+%    .save_all  - save the entire workspace/variables       : 1
 
+
+% default settings
 if ~ exist('meas_in','var') || isempty(meas_in)
 	listing = dir([pwd '/*.out']);
     if ~isempty(listing)
@@ -52,8 +57,8 @@ if ~ exist('options','var') || isempty(options)
     options = [];
 end
 
-if ~ isfield(options,'ref_coi')
-    options.ref_coi = 4;
+if ~ isfield(options,'ref_coil')
+    options.ref_coil = 1;
 end
 
 if ~ isfield(options,'eig_rad')
@@ -73,12 +78,25 @@ if ~ isfield(options,'ph_unwrap')
     % another option is 'bestpath'
 end
 
+if ~ isfield(options,'bkg_rm')
+    options.bkg_rm = {'resharp','lbv'};
+    % options.bkg_rm = {'pdf','sharp','resharp','lbv'};
+end
+
+if ~ isfield(options,'t_svd')
+    options.t_svd = 0.05;
+end
+
 if ~ isfield(options,'smv_rad')
-    options.smv_rad = 3;
+    options.smv_rad = 4;
 end
 
 if ~ isfield(options,'tik_reg')
     options.tik_reg = 5e-4;
+end
+
+if ~ isfield(options,'lbv_layer')
+    options.lbv_layer = 2;
 end
 
 if ~ isfield(options,'tv_reg')
@@ -107,25 +125,26 @@ else
     end
 end
 
-ref_coi   = options.ref_coi;
+ref_coil  = options.ref_coil;
 eig_rad   = options.eig_rad;
 bet_thr   = options.bet_thr;
 ph_unwrap = options.ph_unwrap;
+bkg_rm    = options.bkg_rm;
+t_svd     = options.t_svd;
 smv_rad   = options.smv_rad;
 tik_reg   = options.tik_reg;
+lbv_layer = options.lbv_layer;
 tv_reg    = options.tv_reg;
 inv_num   = options.inv_num;
 save_all  = options.save_all;
 
-
 % define directories
-[~,name] = fileparts(filename);
 if strcmpi(ph_unwrap,'prelude')
-    path_qsm = [path_out, filesep, 'QSM_SWI15_v5_' name];
+    path_qsm = [path_out, filesep, 'QSM_SWI15_v5_pre'];
 elseif strcmpi(ph_unwrap,'laplacian')
-    path_qsm = [path_out, filesep, 'QSM_SWI15_v5_lap_' name];
+    path_qsm = [path_out, filesep, 'QSM_SWI15_v5_lap'];
 elseif strcmpi(ph_unwrap,'bestpath')
-    path_qsm = [path_out, filesep, 'QSM_SWI15_v5_best_' name];
+    path_qsm = [path_out, filesep, 'QSM_SWI15_v5_best'];
 end
 mkdir(path_qsm);
 init_dir = pwd;
@@ -182,7 +201,7 @@ end
 
 % combine RF coils
 if size(img,4) > 1
-    img_cmb = coils_cmb(img,voxelSize,ref_coi,eig_rad);
+    img_cmb = coils_cmb(img,voxelSize,ref_coil,eig_rad);
 else
     img_cmb = img;
 end
@@ -268,16 +287,17 @@ elseif strcmpi('bestpath',ph_unwrap)
     nii = make_nii(unph,voxelSize);
     save_nii(nii,'unph.nii');
 
-    % fid = fopen('reliability.dat','r');
-    % reliability = fread(fid,'float');
-    % fclose(fid);
-    % reliability = reshape(reliability,[nv,np,ns]);
-    % reliability = 1./reliability.*mask;
-    % reliability_smooth = smooth3(reliability,'box',[7,7,3]);
-    % % reliability(reliability <= 0.05) = 0;
-    % % reliability(reliability > 0.05) = 1;
-    % nii = make_nii(reliability_smooth,voxelSize);
-    % save_nii(nii,'reliability_smooth.nii');
+    fid = fopen('reliability.dat','r');
+    reliability = fread(fid,'float');
+    fclose(fid);
+    reliability = reshape(reliability,[nv,np,ns]);
+    reliability = 1./reliability.*mask;
+    % reliability_smooth = smooth3(reliability,'gaussian',[7,7,3],1);
+    reliability(reliability <= 0.1) = 0;
+    reliability(reliability > 0.1) = 1;
+    nii = make_nii(reliability,voxelSize);
+    save_nii(nii,'reliability.nii');
+
 else
     error('what unwrapping methods to use? prelude or laplacian or bestpath?')
 end
@@ -290,46 +310,99 @@ gamma = 2.675222e8;
 tfs = unph/(gamma*TE*B_0)*1e6; % unit ppm
 
 
-% background field removal
-% (1) RESHARP
-[lfs_resharp,mask_resharp] = resharp(tfs,mask,voxelSize,smv_rad,tik_reg);
-mkdir('RESHARP');
-nii = make_nii(lfs_resharp,voxelSize);
-save_nii(nii,'RESHARP/lfs_resharp.nii');
+% PDF
+if sum(strcmpi('pdf',bkg_rm))
+    disp('--> PDF to remove background field ...');
+    [lfs_pdf,mask_pdf] = pdf(tfs,mask,voxelSize,smv_rad, ...
+        abs(img_cmb),z_prjs);
+    % 3D 2nd order polyfit to remove any residual background
+    lfs_pdf= poly3d(lfs_pdf,mask_pdf);
 
-% (2) LBV
-lfs_lbv = LBV(tfs,mask,size(unph),voxelSize,0.01,2); % strip 2 layers
-mkdir('LBV');
-nii = make_nii(lfs_lbv,voxelSize);
-save_nii(nii,'LBV/lfs_lbv.nii');
-% % Don't use LBV's mask*.bin, not accurate
-% % read in eroded mask from LBV
-% listing = dir('mask*.bin');
-% filename = listing.name;
-% fid = fopen(filename);
-% mask_lbv = fread(fid,'int');
-% mask_lbv = reshape(mask_lbv,size(mask));
-% fclose all;
-mask_lbv = ones(size(mask));
-mask_lbv(lfs_lbv==0) = 0;
+    % save nifti
+    mkdir('PDF');
+    nii = make_nii(lfs_pdf,voxelSize);
+    save_nii(nii,'PDF/lfs_pdf.nii');
+
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on PDF...');
+    sus_pdf = tvdi(lfs_pdf, mask_pdf, voxelSize, tv_reg, ...
+        abs(img_cmb), z_prjs, inv_num); 
+
+    % save nifti
+    nii = make_nii(sus_pdf.*mask_pdf,voxelSize);
+    save_nii(nii,'PDF/sus_pdf.nii');
+end
 
 
-% susceptibility inversion
-% (1) RESHARP
-[sus_resharp,residual_resharp] = tvdi(lfs_resharp,mask_resharp,voxelSize, ...
-    tv_reg,abs(img_cmb),z_prjs,inv_num);
-nii = make_nii(sus_resharp,voxelSize);
-save_nii(nii,'RESHARP/sus_resharp.nii');
-nii=make_nii(sus_resharp.*mask_resharp,voxelSize);
-save_nii(nii,'RESHARP/sus_resharp_clean.nii');
+% SHARP (t_svd: truncation threthold for t_svd)
+if sum(strcmpi('sharp',bkg_rm))
+    disp('--> SHARP to remove background field ...');
+    [lfs_sharp, mask_sharp] = sharp(tfs,mask,voxelSize,smv_rad,t_svd);
+    % 3D 2nd order polyfit to remove any residual background
+    lfs_sharp= poly3d(lfs_sharp,mask_sharp);
 
-% (2) LBV
-[sus_lbv,residual_lbv] = tvdi(lfs_lbv,mask_lbv,voxelSize,tv_reg, ...
-    abs(img_cmb),z_prjs,inv_num);
-nii = make_nii(sus_lbv,voxelSize);
-save_nii(nii,'LBV/sus_lbv.nii');
-nii=make_nii(sus_lbv.*mask_lbv,voxelSize);        
-save_nii(nii,'LBV/sus_lbv_clean.nii');  
+    % save nifti
+    mkdir('SHARP');
+    nii = make_nii(lfs_sharp,voxelSize);
+    save_nii(nii,'SHARP/lfs_sharp.nii');
+    
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on SHARP...');
+    sus_sharp = tvdi(lfs_sharp, mask_sharp, voxelSize, tv_reg, ...
+        abs(img_cmb), z_prjs, inv_num); 
+   
+    % save nifti
+    nii = make_nii(sus_sharp.*mask_sharp,voxelSize);
+    save_nii(nii,'SHARP/sus_sharp.nii');
+end
+
+
+% RE-SHARP (tik_reg: Tikhonov regularization parameter)
+if sum(strcmpi('resharp',bkg_rm))
+    disp('--> RESHARP to remove background field ...');
+    [lfs_resharp, mask_resharp] = resharp(tfs,mask,voxelSize,smv_rad,tik_reg);
+    % 3D 2nd order polyfit to remove any residual background
+    lfs_resharp= poly3d(lfs_resharp,mask_resharp);
+
+    % save nifti
+    mkdir('RESHARP');
+    nii = make_nii(lfs_resharp,voxelSize);
+    save_nii(nii,'RESHARP/lfs_resharp.nii');
+
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on RESHARP...');
+    sus_resharp = tvdi(lfs_resharp, mask_resharp, voxelSize, tv_reg, ...
+        abs(img_cmb), z_prjs, inv_num); 
+   
+    % save nifti
+    nii = make_nii(sus_resharp.*mask_resharp,voxelSize);
+    save_nii(nii,'RESHARP/sus_resharp.nii');
+end
+
+
+% LBV
+if sum(strcmpi('lbv',bkg_rm))
+    disp('--> LBV to remove background field ...');
+    lfs_lbv = LBV(tfs,mask,size(tfs),voxelSize,0.01,lbv_layer); % strip 2 layers
+    mask_lbv = ones(size(mask));
+    mask_lbv(lfs_lbv==0) = 0;
+    % 3D 2nd order polyfit to remove any residual background
+    lfs_lbv= poly3d(lfs_lbv,mask_lbv);
+
+    % save nifti
+    mkdir('LBV');
+    nii = make_nii(lfs_lbv,voxelSize);
+    save_nii(nii,'LBV/lfs_lbv.nii');
+
+    % inversion of susceptibility 
+    disp('--> TV susceptibility inversion on lbv...');
+    sus_lbv = tvdi(lfs_lbv,mask_lbv,voxelSize,tv_reg, ...
+        abs(img_cmb),z_prjs,inv_num);   
+
+    % save nifti
+    nii = make_nii(sus_lbv.*mask_lbv,voxelSize);
+    save_nii(nii,'LBV/sus_lbv.nii');
+end
 
 
 % save all variables for debugging purpose
