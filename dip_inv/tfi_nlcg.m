5517bd03854730232878d4f0aa01ea85e2274d8c
Hongfu Sun <sunhongfu@gmail.com>
2016-08-29 18:26:48 -0600

    modified:   dip_inv/nlcg.m 	modified:   dip_inv/tfi_nlcg.m (1) pad 10 zero-slices each side before everything in tfi_nlcg.m (2) change the weighting term in nlcg.m, so now it's in the squared form (consistant with nlcg_singlestep.m)

--- dip_inv/tfi_nlcg.m
+++ dip_inv/tfi_nlcg.m
@@ -1,7 +1,24 @@
-function [chi, res] = tfi_nlcg(tfs, Res_wt, sus_mask, Tik_mask, TV_mask, Tik_reg, TV_reg, vox, z_prjs, Itnlim)
+function [chi, res, Res_term, TV_term, Tik_term] = tfi_nlcg(tfs, Res_wt, sus_mask, Tik_mask, TV_mask, Tik_reg, TV_reg, vox, z_prjs, Itnlim)
 
 % argmin ||Res_wt * (F_{-1} * D * F * sus_mask * chi - tfs)|| + Tik_reg*||Tik_mask * chi|| + TV_reg*TV(TV_mask * chi)
 
+
+
+
+% pad extra 10 slices on both sides
+tfs = padarray(tfs,[0 0 10]);
+Res_wt = padarray(Res_wt,[0 0 10]);
+sus_mask = padarray(sus_mask,[0 0 10]);
+Tik_mask = padarray(Tik_mask,[0 0 10]);
+TV_mask = padarray(TV_mask,[0 0 10]);
+
+if ~ exist('z_prjs','var') || isempty(z_prjs)
+    z_prjs = [0, 0, 1]; % PURE axial slices
+end
+
+if ~ exist('Itnlim','var') || isempty(Itnlim)
+    Itnlim = 500;
+end
 
 % if ~ exist('weights','var') || isempty(weights)
 %     weights = mask_b;
@@ -39,7 +56,7 @@ params.FT = cls_dipconv([Nx,Ny,Nz],D);
 params.TV = cls_tv;
 
 params.Itnlim = Itnlim; % interations numbers (adjust accordingly!)
-params.gradToll = 1e-6; % step size tolerance stopping criterea
+params.gradToll = 1e-4; % step size tolerance stopping criterea
 params.l1Smooth = eps; %1e-15; smoothing parameter of L1 norm
 params.pNorm = 1; % type of norm to use (i.e. L1 L2 etc)
 params.lineSearchItnlim = 100;
@@ -64,6 +81,10 @@ params.data = tfs;
 chi = real(chi);
 
 % residual difference between fowardly calculated field and lfs
-res = tfs - real(ifftn(D.*fftn(chi)));
+res = Res_wt.*(tfs - real(ifftn(D.*fftn(sus_mask.*chi))));
 
+% remove the extra padding slices
+chi = chi(:,:,11:end-10);
+res = res(:,:,11:end-10);
 
+end
