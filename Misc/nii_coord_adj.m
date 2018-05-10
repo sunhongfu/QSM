fb572202be31380851156393089bf496b2b7618a
hongfu <sunhongfu@gmail.com>
2018-05-11 07:14:39 +1000

    add nii_coord_adj.m file to properly reorient the NIFTI and add proper hdr derived from DICOM images

---
+++ Misc/nii_coord_adj.m
@@ -0,0 +1,21 @@
+function nii_coord_adj(nii_input_name, nii_ref_name)
+
+nii_input = load_untouch_nii(nii_input_name);
+nii_ref = load_untouch_nii(nii_ref_name);
+
+nii_output = nii_ref;
+nii_output.img = (flipdim(flipdim(single(nii_input.img),1),2));
+
+nii_output.hdr.dime.datatype = 16;
+nii_output.hdr.dime.bitpix = 32;
+
+nii_output.hdr.dime.glmax = max(nii_input.img(:));
+nii_output.hdr.dime.glmin = min(nii_input.img(:));
+nii_output.hdr.dime.dim = nii_input.hdr.dime.dim;
+
+[filepath,name,ext] = fileparts(nii_input_name);
+nii_output_name = [filepath filesep name '_adj' ext];
+
+save_untouch_nii(nii_output,nii_output_name);
+
+
