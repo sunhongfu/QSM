9d1d5a6c4e4fdc9fbe2daf00c677e155efb4323a
Hongfu Sun <sunhongfu@gmail.com>
2018-05-17 15:48:48 -0600

    add 4th order polynomials to poly3d.m

--- background_field_removal/poly3d.m
+++ background_field_removal/poly3d.m
@@ -35,6 +35,16 @@ elseif poly_order == 3
 	P_nz = [px_nz.^3, py_nz.^3, pz_nz.^3, px_nz.*py_nz.^2, px_nz.*pz_nz.^2, px_nz.*py_nz.*pz_nz, py_nz.*px_nz.^2, py_nz.*pz_nz.^2, pz_nz.*px_nz.^2, pz_nz.*py_nz.^2 ...
 		px_nz.^2, py_nz.^2, pz_nz.^2, px_nz.*py_nz, px_nz.*pz_nz, py_nz.*pz_nz, px_nz, py_nz, pz_nz, ones(length(px_nz),1)]; % polynomials
 
+
+elseif poly_order == 4
+	% third order
+	P = [px.^4, py.^4, pz.^4, px.^3.*py, px.^3.*pz, py.^3.*px, py.^3.*pz, pz.^3.*px, pz.^3.*py, px.^2.*py.^2, px.^2.*pz.^2, px.^2.*py.*pz, py.^2.*pz.^2, py.^2.*px.*pz, pz.^2.*px.*py ...
+		px.^3, py.^3, pz.^3, px.*py.^2, px.*pz.^2, px.*py.*pz, py.*px.^2, py.*pz.^2, pz.*px.^2, pz.*py.^2 ...
+		px.^2, py.^2, pz.^2, px.*py, px.*pz, py.*pz, px, py, pz, ones(length(px),1)]; % polynomials
+	P_nz = [px_nz.^4, py_nz.^4, pz_nz.^4, px_nz.^3.*py_nz, px_nz.^3.*pz_nz, py_nz.^3.*px_nz, py_nz.^3.*pz_nz, pz_nz.^3.*px_nz, pz_nz.^3.*py_nz, px_nz.^2.*py_nz.^2, px_nz.^2.*pz_nz.^2, px_nz.^2.*py_nz.*pz_nz, py_nz.^2.*pz_nz.^2, py_nz.^2.*px_nz.*pz_nz, pz_nz.^2.*px_nz.*py_nz ...
+		px_nz.^3, py_nz.^3, pz_nz.^3, px_nz.*py_nz.^2, px_nz.*pz_nz.^2, px_nz.*py_nz.*pz_nz, py_nz.*px_nz.^2, py_nz.*pz_nz.^2, pz_nz.*px_nz.^2, pz_nz.*py_nz.^2 ...
+		px_nz.^2, py_nz.^2, pz_nz.^2, px_nz.*py_nz, px_nz.*pz_nz, py_nz.*pz_nz, px_nz, py_nz, pz_nz, ones(length(px_nz),1)]; % polynomials
+
 else
 	error('cannot do higher than 3rd order');
 end
