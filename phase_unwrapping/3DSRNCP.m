8610959be80ccc901617f9ce2af1458ef2092d6f
Hongfu Sun <sunhongfu@gmail.com>
2023-11-03 12:25:06 +1000

    Update 3DSRNCP.m

--- phase_unwrapping/3DSRNCP.m
+++ phase_unwrapping/3DSRNCP.m
@@ -1,2 +1,4 @@
 % empty file, just to locate the actual c++ execuatable
 
+% remember to rename ‘3DSRNCP_linux’ or ‘3DSRNCP_mac’ to ‘3DSRNCP’ depending on your platform. by default the current '3DSRNCP' is for linux platform.
+% if you use mac, override it by changing name ‘3DSRNCP_mac’ to ‘3DSRNCP’
